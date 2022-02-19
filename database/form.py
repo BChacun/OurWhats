f07639d8cc1b40ef1a6d9352ad1719dad0430522
Bats <baptiste.chacun@imt-atlantique.net>
2022-02-19 01:27:13 +0100

    not_same_username

--- database/form.py
+++ database/form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField, validators
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
+import database.models as models
 
 
 class SignupForm(FlaskForm):
@@ -20,3 +21,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = models.User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
